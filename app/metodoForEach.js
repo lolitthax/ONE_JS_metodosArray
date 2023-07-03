51e6da2df3dfdb7ff971b90f209236ec59b81026
Aline Damião <aline.isilvadamiao@gmail.com>
2023-07-03 16:15:24 -0300

    Livros indisponíveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,11 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
   elementoParaInserirLivros.innerHTML = '';
   listaDeLivros.forEach(livro => {
+    // let disponibilidade = verficarDisponibilidadedoLivro(livro);
+    let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'; //usando operador para verificar disponibilidade com apenas uma linha com operador ternário
     elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
+        <img class="${disponibilidade}" src="${livro.imagem}"
           alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
@@ -19,3 +21,12 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
   })
 }
+
+//Função para verficar disponibilidade
+// function verficarDisponibilidadedoLivro(livro) {
+//   if (livro.quantidade > 0) {
+//     return 'livro__imagens';
+//   } else {
+//     return 'livros__imagens indisponivel';
+//   }
+// }
